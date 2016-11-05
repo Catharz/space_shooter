66b85be920e85bbf18a2da7a05b6ad829c7d6123
unknown <craig@CraigsUltra7.(none)>
2016-11-05 12:47:24 +1100

    initial filecommit

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	public SimpleTouchPad touchPad;
+	public SimpleTouchFireButton fireButton;
+	 
+	private float nextFire;
+	private Quaternion calibrationQuaternion;
+
+	#if UNITY_IPHONE
+	void Start ()
+	{
+		CalibrateAccelerometer ();
+	}
+	#endif
+
+	void Update ()
+	{
+		#if UNITY_IPHONE
+		bool canFire = (fireButton.CanFire () && Time.time > nextFire);
+		#else
+		bool canFire = (Input.GetButton ("Fire1") && Time.time > nextFire);
+		#endif
+		if (canFire && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			GetComponent<AudioSource> ().Play ();
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		#if UNITY_IPHONE
+		Vector2 direction = touchPad.GetDirection ();
+		Vector3 movement = new Vector3 (direction.x, 0.0f, direction.y);
+		#else
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		#endif
+
+		GetComponent<Rigidbody> ().velocity = movement * speed;
+
+		GetComponent<Rigidbody> ().position = new Vector3 (
+			Mathf.Clamp (GetComponent<Rigidbody> ().position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp (GetComponent<Rigidbody> ().position.z, boundary.zMin, boundary.zMax)
+		);
+
+		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody> ().velocity.x * -tilt);
+	}
+
+	#if UNITY_IPHONE
+	void CalibrateAccelerometer ()
+	{
+		Vector3 accelerationSnapshot = Input.acceleration;
+		Quaternion rotateQuaternion = Quaternion.FromToRotation (new Vector3 (0.0f, 0.0f, -1.0f), accelerationSnapshot);
+		calibrationQuaternion = Quaternion.Inverse (rotateQuaternion);
+	}
+
+	Vector3 FixAcceleration (Vector3 acceleration)
+	{
+		Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
+		return fixedAcceleration;
+	}
+	#endif
+}
---
+++ Assets/_Scripts/SimpleTouchFireButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class SimpleTouchFireButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+	private bool touched;
+	private int pointerId;
+	private bool canFire;
+
+	void Awake ()
+	{
+		touched = false;
+	}
+
+	public void OnPointerDown (PointerEventData data)
+	{
+		if (!touched) {
+			touched = true;
+			pointerId = data.pointerId;
+			canFire = true;
+		}
+	}
+
+	public void OnPointerUp (PointerEventData data)
+	{
+		if (data.pointerId == pointerId) {
+			touched = false;
+			canFire = false;
+		}
+	}
+
+	public bool CanFire ()
+	{
+		return canFire;
+	}
+}
